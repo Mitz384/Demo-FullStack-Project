0babfbc0a86300bd56d87bc8a1c1b1b822ef9c3d
Mitz308 <dnson384@gmail.com>
2025-07-11 20:48:41 +0700

    fix: sửa lỗi DB test

--- SERVER/db_scripts/init_test_db.sql
+++ SERVER/db_scripts/init_test_db.sql
@@ -19,7 +19,7 @@ CREATE TABLE products (
     description TEXT,
     price INTEGER NOT NULL,
     stock INTEGER NOT NULL DEFAULT 0,
-    image_url VARCHAR(255)
+    image VARCHAR(255)
 );
 
 INSERT INTO users (first_name, last_name, email, password) VALUES
@@ -28,6 +28,6 @@ INSERT INTO users (first_name, last_name, email, password) VALUES
 ( 'Doan', 'Nhu', 'doannhuson2004@gmail.com', '$2b$10$6e8dgGxUVkPJLX4qDMOBFencxhSJ9HBK.Aq1d//Yw6GI3PmWGFLZO' );
 
 
-INSERT INTO products (name, description, price, stock, image_url) VALUES
+INSERT INTO products (name, description, price, stock, image) VALUES
 ( 'Áo thun nam cotton', 'Áo thun chất cotton thoáng mát, co giãn tốt', 199000, 50, '/images/ao-thun-nam.jpg' ),
 ( 'Quần jean nữ', 'Quần jean lưng cao thời trang, tôn dáng', 349000, 30, '/images/quan-jean-nu.jpg' );
