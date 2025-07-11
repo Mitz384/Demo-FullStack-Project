f544ca270cb61b7d52aee91577962458c97db6e0
Mitz308 <dnson384@gmail.com>
2025-07-11 19:25:13 +0700

    fix: thêm env.NODE_ENV: test -> không test khi login bằng Google

--- SERVER/config/auth.js
+++ SERVER/config/auth.js
@@ -2,35 +2,37 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const { createUserWithGoogle } = require("../services/userService");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/api/users/auth/google/callback",
-      passReqToCallback: true,
-    },
-    async function (request, accessToken, refreshToken, profile, done) {
-      const firstName = profile.name.givenName || "";
-      const lastName = profile.name.familyName || "";
-      const email =
-        profile.emails && profile.emails.length > 0
-          ? profile.emails[0].value
-          : null;
+if (process.env.NODE_ENV !== "test") {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:8080/api/users/auth/google/callback",
+        passReqToCallback: true,
+      },
+      async function (request, accessToken, refreshToken, profile, done) {
+        const firstName = profile.name.givenName || "";
+        const lastName = profile.name.familyName || "";
+        const email =
+          profile.emails && profile.emails.length > 0
+            ? profile.emails[0].value
+            : null;
 
-      if (!email) {
-        return done(
-          new Error("Cannot sign in to Google: No email provided"),
-          null
+        if (!email) {
+          return done(
+            new Error("Cannot sign in to Google: No email provided"),
+            null
+          );
+        }
+
+        createUserWithGoogle({ firstName, lastName, email, done }).catch(
+          (err) => {
+            console.error("Error creating user with Google:", err);
+            return done(err, null);
+          }
         );
       }
-
-      createUserWithGoogle({ firstName, lastName, email, done }).catch(
-        (err) => {
-          console.error("Error creating user with Google:", err);
-          return done(err, null);
-        }
-      );
-    }
-  )
-);
+    )
+  );
+}
