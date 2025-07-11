67750b8489c729fb3eb0854c1a9fe9b817a39b60
Mitz308 <dnson384@gmail.com>
2025-07-11 18:11:03 +0700

    ci: thêm cấu hình Github Actions để test backend với PostgreSQL

--- SERVER/config/db.js
+++ SERVER/config/db.js
@@ -2,10 +2,10 @@ const { Client } = require("pg");
 
 const connection = new Client({
   host: process.env.DB_HOST,
-  user: "postgres",
-  password: "300804",
-  port: 5432,
-  database: "DOPDOPDOP",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 connection._connected = false;
